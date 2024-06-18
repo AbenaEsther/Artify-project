5d01578b529863c1b1520afde65803c3165073c7
AbenaEsther <essieofosina78@gmail.com>
2024-06-18 14:07:03 +0000

    feat/signUp

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,20 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../shared/services/interfaces';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
+signupForm= new FormGroup({
+  fullname:new FormControl('',[Validators.required]),
+  email: new FormControl('',[Validators.required, Validators.email]),
+  phonenumber: new FormControl('', [Validators.required]),
+  password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  repeatPassword: new FormControl('', [Validators.required])
+})
 
-  ngOnInit(): void {
-    const Form= document.querySelector("form");
+get password(){
+  return this.signupForm.controls['password']
+}
 
-    Form?.addEventListener("submit", (e)=>{
-      e.preventDefault();
-      const formData = new FormData(Form);
-    })
-  }
+get invalidPassword(){
+  return this.password.invalid && this.password.dirty;
+}
+
+get email(){
+  return this.signupForm.controls['email']
+}
+
+get invalidEmail(){
+  return this.email.invalid && this.password.dirty;
+}
+
+get repeatPassword(){
+  return this.signupForm.controls['repeatPassword']
+}
+
+get invalidRepeatPassword(){
+  return this.repeatPassword.invalid && this.repeatPassword.dirty
+}
+
+get phonenumber(){
+  return this.signupForm.controls['phonenumber']
+}
+
+get invalidPhonenumber(){
+  return this.phonenumber.invalid && this.phonenumber.dirty
+}
+
+
+
+createAccount(){
+  // const user= {
+  //   email: this.signupForm.value.email,
+  //   password: this.signupForm.value.password
+  // } as User;
+
+  if (!this.signupForm.valid) return;
+}
 }
