e3b3d32dcacad9c805b0ed2ae4947b58dce43ad7
AbenaEsther <essieofosina78@gmail.com>
2024-06-21 15:11:40 +0000

    style/signUp

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,83 +1,114 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { User } from '../shared/services/interfaces';
 import { passwordMatchValidator } from '../shared/services/password-mismatch';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule,CommonModule,ReactiveFormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {
-signupForm= new FormGroup({
-  fullname:new FormControl('',[Validators.required]),
-  email: new FormControl('',[Validators.required, Validators.email]),
-  phonenumber: new FormControl('', [Validators.required]),
-  password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-  confirmPassword: new FormControl('', [Validators.required])
-},
-{
-  validators:passwordMatchValidator
-}
-)
+export class SignUpComponent implements OnInit {
+  signupForm = new FormGroup(
+    {
+      fullname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phonenumber: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    {
+      validators: passwordMatchValidator,
+    }
+  );
 
-get password(){
-  return this.signupForm.controls['password']
-}
+  get password() {
+    return this.signupForm.controls['password'];
+  }
 
-get invalidPassword(){
-  return this.password.invalid && this.password.dirty;
-}
+  get invalidPassword() {
+    return this.password.invalid && this.password.dirty;
+  }
 
-get email(){
-  return this.signupForm.controls['email']
-}
+  get email() {
+    return this.signupForm.controls['email'];
+  }
 
-get invalidEmail(){
-  return this.email.invalid && this.email.dirty;
-}
+  get invalidEmail() {
+    return this.email.invalid && this.email.dirty;
+  }
 
-get confirmPassword(){
-  return this.signupForm.controls['confirmPassword']
-}
+  get confirmPassword() {
+    return this.signupForm.controls['confirmPassword'];
+  }
 
-get invalidConfirmPassword(){
-  return this.confirmPassword.invalid && this.confirmPassword.dirty
-}
+  get invalidConfirmPassword() {
+    return this.confirmPassword.invalid && this.confirmPassword.dirty;
+  }
 
-get phonenumber(){
-  return this.signupForm.controls['phonenumber']
-}
+  get phonenumber() {
+    return this.signupForm.controls['phonenumber'];
+  }
 
-get invalidPhonenumber(){
-  return this.phonenumber.invalid && this.phonenumber.dirty
-}
+  get invalidPhonenumber() {
+    return this.phonenumber.invalid && this.phonenumber.dirty;
+  }
 
-get fullname(){
-  return this.signupForm.controls['fullname']
-}
+  get fullname() {
+    return this.signupForm.controls['fullname'];
+  }
 
-get invalidFullname(){
-  return this.fullname.invalid && this.fullname.dirty
-}
+  get invalidFullname() {
+    return this.fullname.invalid && this.fullname.dirty;
+  }
 
-get misMatchPassword(){
-  return (
-    this.signupForm.errors?.['passwordMismatch'] && this.confirmPassword.valid && this.password.valid
-  )
-}
+  get misMatchPassword() {
+    return (
+      this.signupForm.errors?.['passwordMismatch'] &&
+      this.confirmPassword.valid &&
+      this.password.valid
+    );
+  }
 
+  createAccount() {
+    // const user= {
+    //   email: this.signupForm.value.email,
+    //   password: this.signupForm.value.password
+    // } as User;
 
+    if (!this.signupForm.valid) return;
+  }
 
-createAccount(){
-  // const user= {
-  //   email: this.signupForm.value.email,
-  //   password: this.signupForm.value.password
-  // } as User;
-
-  if (!this.signupForm.valid) return;
-}
+  ngOnInit(): void {
+    let eyeicon = document.getElementById('eyeicon') as HTMLImageElement;
+    let password = document.getElementById('password')as HTMLInputElement;
+    let confirmPassword= document.getElementById('confirm-password') as HTMLInputElement
+   
+    eyeicon!.onclick = function () {
+      if (password!.type == 'password') {
+        password!.type = 'text';
+        eyeicon!.src = '/assets/images/eye-close.png';
+      } else if(confirmPassword!.type =='password'){
+        password!.type = 'text';
+        eyeicon!.src = '/assets/images/eye-close.png';
+        console.log('confirm-password is visible')
+      }
+      else {
+        password!.type = 'password';
+        eyeicon!.src = '/assets/images/eye-open.png';
+      }
+    };
+  }
 }
